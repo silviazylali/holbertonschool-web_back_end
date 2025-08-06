c96e463292bd9f89858145c65d598e37ebc12d74
silviazylali <silviazylali0707@gmail.com>
2025-08-06 15:22:28 +0000

    Exercise 6 added

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => (result.status === 'fulfilled'
+      ? { status: result.status, value: result.value }
+      : { status: result.status, value: result.reason.toString() })))
+    .catch((error) => error);
+}
