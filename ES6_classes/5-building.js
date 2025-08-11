0af2844df29d17604c1d86e64b3a0f651250042e
silviazylali <silviazylali0707@gmail.com>
2025-08-11 12:41:00 +0000

    Checked

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (this.constructor === Building) {
-      return;
-    }
-
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
