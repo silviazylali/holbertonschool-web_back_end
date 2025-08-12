4e007c2923fdf26b1f36aa4c9a02cc807955b75d
silviazylali <silviazylali0707@gmail.com>
2025-08-12 14:18:32 +0000

    Task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,14 +2,14 @@ export default function updateStudentGradeByCity(listOfStudents, city, newGrades
   const students = listOfStudents.filter((student) => student.location === city);
   const updatedStudent = students.map((student) => {
     const grades = newGrades.find((grade) => grade.studentId === student.id);
-  
+
     const grade = grades ? grades.grade : 'N/A';
-  
+
     return {
       ...student,
       grade,
     };
   });
-  
+
   return updatedStudent;
 }
